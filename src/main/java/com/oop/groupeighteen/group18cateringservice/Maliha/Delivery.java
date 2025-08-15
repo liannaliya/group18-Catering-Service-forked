22f61fcf7376c7e5a51291657132151fa03448d5
mali213 <155835618+mali213@users.noreply.github.com>
2025-08-16 00:57:14 +0600

    addAttribute

--- src/main/java/com/oop/groupeighteen/group18cateringservice/Maliha/Delivery.java
+++ src/main/java/com/oop/groupeighteen/group18cateringservice/Maliha/Delivery.java
@@ -1,7 +1,9 @@
 package com.oop.groupeighteen.group18cateringservice.Maliha;
 
 public class Delivery
+    
 {
+    private String getName;
     @javafx.fxml.FXML
     public void initialize() {
     }}
