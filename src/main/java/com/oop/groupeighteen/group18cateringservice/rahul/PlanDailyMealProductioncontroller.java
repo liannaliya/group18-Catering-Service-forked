cd5eef371390a99cbd92acc965a5929cf1bd3451
Rahul Bepari <rahulbepari35@gamil.com>
2025-08-16 11:11:29 +0600

    Catering Manager screen switch

--- src/main/java/com/oop/groupeighteen/group18cateringservice/rahul/PlanDailyMealProductioncontroller.java
+++ src/main/java/com/oop/groupeighteen/group18cateringservice/rahul/PlanDailyMealProductioncontroller.java
@@ -1,10 +1,18 @@
 package com.oop.groupeighteen.group18cateringservice.rahul;
 
+import com.oop.groupeighteen.group18cateringservice.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PlanDailyMealProductioncontroller
 {
@@ -31,5 +39,17 @@ public class PlanDailyMealProductioncontroller
 
     @javafx.fxml.FXML
     public void generatepreplistAB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backAB(ActionEvent actionEvent) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("rahul/CateringManagerDashboard.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Catering Manager Dashboard");
+        stage.show();
     }
 }
--- src/main/java/com/oop/groupeighteen/group18cateringservice/rahul/SubmitEndofDayReportcontroller.java
+++ src/main/java/com/oop/groupeighteen/group18cateringservice/rahul/SubmitEndofDayReportcontroller.java
@@ -1,8 +1,16 @@
 package com.oop.groupeighteen.group18cateringservice.rahul;
 
+import com.oop.groupeighteen.group18cateringservice.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SubmitEndofDayReportcontroller
 {
@@ -23,5 +31,17 @@ public class SubmitEndofDayReportcontroller
 
     @javafx.fxml.FXML
     public void selectFIleAB(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backAB(ActionEvent actionEvent) throws IOException {
+        Parent root = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("rahul/CateringManagerDashboard.fxml"));
+        root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Catering Manager Dashboard");
+        stage.show();
     }
 }
